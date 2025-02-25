4b1fe3ef79c716516b084dc1703f40644e251a2b
Karandaev Ruslan <karandaevruslan@gmail.com>
2025-02-25 11:30:54 +0700

    Created empty cmake project

---
+++ MultidimensionalGeometricEditor/MultidimensionalGeometricEditor.cpp
@@ -0,0 +1,12 @@
+﻿// MultidimensionalGeometricEditor.cpp : Defines the entry point for the application.
+//
+
+#include "MultidimensionalGeometricEditor.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
