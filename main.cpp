df0e66439ad7664b03f582c5d31f7de9b7cfdb17
Ruslan Karandaev <karandaevruslan@gmail.com>
2025-05-03 11:18:05 +0700

    Fixed old style

--- main.cpp
+++ main.cpp
@@ -4,6 +4,11 @@
 #include <QCommandLineParser>
 #include <QCommandLineOption>
 #include <QOpenGLWindow>
+#include <QStyleFactory>
+#include <QOperatingSystemVersion>
+#include <QSysInfo>
+#include <QProcess>
+#include <QDebug>
 
 #include "tools/configManager.h"
 #include "tools/logger.h"
@@ -11,10 +16,94 @@
 #include "model/sceneColorificator.h"
 #include "presenterMain.h"
 
+QString detectLinuxDesktopEnvironment()
+{
+#ifdef Q_OS_LINUX
+    QByteArray xdg = qgetenv("XDG_CURRENT_DESKTOP");
+    if (!xdg.isEmpty()) {
+        QString env = QString::fromUtf8(xdg).toLower();
+        if (env.contains("kde"))       return "KDE";
+        if (env.contains("gnome"))     return "GNOME";
+        if (env.contains("xfce"))      return "XFCE";
+        if (env.contains("cinnamon"))  return "Cinnamon";
+        if (env.contains("lxde"))      return "LXDE";
+    }
+    QByteArray session = qgetenv("DESKTOP_SESSION");
+    if (!session.isEmpty())
+        return QString::fromUtf8(session).toLower();
+#endif
+    return QString();
+}
+
+QString chooseWindowsStyle(const QStringList &availableStyles)
+{
+#if QT_VERSION_MAJOR >= 6
+    constexpr bool isQt6 = true;
+#else
+#endif
+
+    auto winVer = QOperatingSystemVersion::current();
+
+    if (isQt6
+        && winVer >= QOperatingSystemVersion::Windows11
+        && availableStyles.contains("windows11", Qt::CaseInsensitive))
+    {
+        return "windows11";
+    }
+    if (winVer >= QOperatingSystemVersion::Windows10
+        && availableStyles.contains("Windows", Qt::CaseInsensitive))
+    {
+        return "Windows";
+    }
+    if (availableStyles.contains("windowsvista", Qt::CaseInsensitive))
+        return "windowsvista";
+    if (availableStyles.contains("Windows", Qt::CaseInsensitive))
+        return "Windows";
+    return QString();
+}
+
+void setAppropriateStyle()
+{
+    QStringList styles = QStyleFactory::keys();
+    qDebug() << "Available styles:" << styles;
+
+    QString chosen;
+#if defined(Q_OS_WIN)
+    chosen = chooseWindowsStyle(styles);
+#elif defined(Q_OS_MAC)
+    chosen = styles.contains("macintosh", Qt::CaseInsensitive)
+                 ? "macintosh" : "Fusion";
+#elif defined(Q_OS_LINUX)
+    QString de = detectLinuxDesktopEnvironment();
+    if (de == "KDE"     && styles.contains("Breeze"))
+        chosen = "Breeze";
+    else if ((de == "GNOME" || de == "Cinnamon")
+             && styles.contains("GTK+"))
+        chosen = "GTK+";
+    else if (styles.contains("Fusion"))
+        chosen = "Fusion";
+    else if (!styles.isEmpty())
+        chosen = styles.first();
+#else
+    chosen = "Fusion";
+#endif
+
+    if (!chosen.isEmpty()) {
+        qDebug() << "Setting style to:" << chosen;
+        QApplication::setStyle(QStyleFactory::create(chosen));
+    } else {
+        qWarning() << "No suitable style found, using default.";
+    }
+}
+
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     app.setWindowIcon(QIcon(":/images/app_icon.png"));
+
+    QString currentStyle = app.style()->objectName();
+    qDebug() << "Default Qt style:" << currentStyle;
+    setAppropriateStyle();
 
     QCoreApplication::setApplicationName("NDEditor");
     QCoreApplication::setOrganizationName("Ruslan Karandaev");
